5cf24d2ea0cba32013993602d1b76e5de29d8346
GabrielNechifor <47883090+GabrielNechifor@users.noreply.github.com>
2019-05-16 10:18:14 +0300

    am sters toate fisierele

--- Payment_Management1/src/main/java/com/paymentManagement/repositories/PaymentsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.paymentManagement.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.paymentManagement.payment.Payments;
-
-public interface PaymentsRepository extends JpaRepository<Payments,Integer>{
-
-}
