23da45bc6258b715a1d45403e6fcb6a54e009ae0
GabrielNechifor <47883090+GabrielNechifor@users.noreply.github.com>
2019-05-17 19:28:19 +0300

    am sters account_management duplicat

--- account_management/src/main/java/com/accountManagement/repositories/ProfilesDriverRepository.java
+++
@@ -1,9 +0,0 @@
-package com.accountManagement.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.accountManagement.model.ProfilesDriver;
-
-public interface ProfilesDriverRepository extends JpaRepository<ProfilesDriver,String>{
-
-}
